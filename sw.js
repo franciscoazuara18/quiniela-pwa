6a9505bd38b5af8c1bfda2a6414dc7833b25cc19
franciscoazuara18 <francisco.azuara18@gmail.com>
2026-08-25 13:26:08 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
  * (partidos, tabla, envíos) siempre se piden en vivo al Sheet, nunca se
  * guardan en caché, porque tienen que estar actualizados.
  */
-const CACHE_NAME = "quiniela-shell-v1";
+const CACHE_NAME = "quiniela-shell-v2";
 const ARCHIVOS_SHELL = [
   "./",
   "./index.html",
